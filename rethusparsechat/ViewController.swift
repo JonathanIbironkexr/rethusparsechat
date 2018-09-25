4839be555fdc74a5996a8bdaa3e40e8a9737f1fa
user142219 <user142219@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-24 21:08:23 -0400

    Initial Commit

---
+++ rethusparsechat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rethusparsechat
+//
+//  Created by user142219 on 9/24/18.
+//  Copyright © 2018 user142219. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
